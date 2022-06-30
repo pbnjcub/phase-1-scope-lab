1923878032cd43ae693abe2a349f12ae6a90de2d
Paul Jung <pbnjcub@gmail.com>
2022-06-29 20:36:12 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+   customerName = 'BOB';
+}
+
+function setBestCustomer() {
+   bestCustomer = 'not bob';  
+}
+
+overwriteBestCustomer= () => bestCustomer = 'maybe bob';
+
+const leastFavoriteCustomer = 'Max';
+
+changeLeastFavoriteCustomer = () => leastFavoriteCustomer = 'Jon';
